0251c556954bf29bcc5bc09a9e65f248affbef8e
Vladimir Matveev <matveev@matller.com>
2022-08-06 18:23:36 +0300

    Json - balans
    - Вычитка баланса из файла

---
+++ Rpg/Balans/GetCharactersBalans.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using RPG.Character;
+using RPG.Character.CharacterCreationFactory;
+
+namespace Rpg.Balans
+{
+    public class GetCharactersBalans
+    {
+        public Balance GetBalans()
+        {
+            Balance balance = JsonConvert.DeserializeObject<Balance>(File.ReadAllText(@"MyJson.json"));
+
+            return balance;
+        }
+
+        public void CreateJsonBalansFromExample()
+        {
+            Balance balance = new Balance()
+            {
+                PlayerBalance = new Dictionary<string, PlayerBalance>()
+                {
+                    ["Player1"] = new PlayerBalance()
+                    {
+                        Stats = Stats.Default 
+                    }
+                },
+                EnemyBalance  = new Dictionary<string, EnemyBalance>()
+                {
+                    ["Enemy1"] = new EnemyBalance()
+                    {
+                        Stats = Stats.Default
+                    }
+                }
+            };
+            
+            string json = JsonConvert.SerializeObject(balance, Formatting.Indented);
+            
+            string path = @"MyJson.json";
+            File.WriteAllText(path, json);
+        }
+    }
+}
--- Rpg/Program.cs
+++ Rpg/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using Newtonsoft.Json;
+using Rpg.Balans;
 using RPG.Character;
 using RPG.Character.CharacterCreationFactory;
 using RPG.Weapons;
@@ -10,72 +11,14 @@ using RPG.Weapons.DamageCalculation;
 
 namespace Rpg
 {
-
-    public class CharactersFromJson
-    {
-        public Dictionary<string, Balance> Characters;
-
-        // public void GenerationJSON()
-        // {
-        //     CharactersFromJson charactersFromJson = new CharactersFromJson()
-        //     {
-        //         Characters = new Dictionary<string, Stats>
-        //         {
-        //             ["Player1"] = new Stats()
-        //             {
-        //                 Power            = 5,
-        //                 Energy           = 5,
-        //                 Agility          = 5,
-        //                 Endurance        = 5,
-        //                 DamageProtection = 5,
-        //                 MaxHealth        = 100
-        //             },
-        //             ["Enemy1"] = new Stats()
-        //             {
-        //                 Power            = 1,
-        //                 Energy           = 1,
-        //                 Agility          = 1,
-        //                 Endurance        = 1,
-        //                 DamageProtection = 1,
-        //                 MaxHealth        = 10
-        //             }
-        //         }
-        //     };
-        //
-        //     string json = JsonConvert.SerializeObject(charactersFromJson, Formatting.None);
-        //     
-        //     string path = @"c:\Users\matve\Documents\_dev\rpg\Rpg\json\MyJson.json";
-        //     File.WriteAllText(path, json);
-        // }
-    }
-
-    
     internal static class Program
     {
         public static void Main(string[] args)
         {
-            CharactersFromJson charactersFromJson = JsonConvert.DeserializeObject<CharactersFromJson>(File.ReadAllText(@"c:\Users\matve\Documents\_dev\rpg\Rpg\json\MyJson.json"));
-
-            Balance balance = new Balance()
-            {
-                PlayerBalance = new Dictionary<string, PlayerBalance>()
-                {
-                    ["Player1"] = new PlayerBalance()
-                    {
-                        Stats = Stats.Default 
-                    }
-                },
-                EnemyBalance  = new Dictionary<string, EnemyBalance>()
-                {
-                    ["Enemy1"] = new EnemyBalance()
-                    {
-                        Stats = Stats.Default
-                    }
-                }
-            };
-
+            Balance balance = new GetCharactersBalans().GetBalans();
+            
             ICharatersFactory MainCharacter = new CharactersFactory(balance, new TestDamageCalculator());
-
+            
             var Player = MainCharacter.CreateCharacter("Player1");
             var Enemy  = MainCharacter.CreateCharacter("Enemy1");
         }
